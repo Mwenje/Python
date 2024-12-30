b6af58f21af133ac257daa35efbde10d75486fe2
Mwenje <samuelmwenje7@gmail.com>
2024-12-30 12:04:59 +0300

    fibonacciSeq

---
+++ fibonacciSeq.js
@@ -0,0 +1,12 @@
+// f(n)=f(n-1)+f(n-2)
+
+function fibSeq(n) {
+  let fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+  return fib;
+}
+
+let fib = fibSeq(10);
+console.log(fib.join(" "));
